94b74e2a57eafe704ee04fed878f1eaf1481ec78
bclbrendan <bclbrendan@gmail.com>
2023-01-27 20:09:20 +0000

    worked out logic for save button, cleaned up CSS tags

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ var today = moment();
 const currentDay = $("#currentDay")
 currentDay.text(today.format("D MMM YYYY"));
 
+$('.saveBtn').on('click', function(event){
+   // const input = event
+    var input = $(this).siblings('.description').val();
+    console.log(input)
+}
+ )
 
-
-
+ var value = $(this).siblings('.description').val();
 
